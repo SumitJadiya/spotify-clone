61b4e6dd0237bc5d540dae1d9204e1b4bd9b93db
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-08-05 23:28:37 +0530

    add spotify web api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { useEffect } from 'react';
+import SpotifyWebApi from "spotify-web-api-js";
 import './App.css';
 import Login from './Login';
 import { getTokenFromResponse } from './spotify';
+import { useState } from 'react';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null)
 
   // Run code on the based of given condition
   useEffect(() => {
     const hash = getTokenFromResponse()
     window.location.hash = ""
-    console.log(hash)
+
+    const _token = hash.access_token;
+
+    if (_token)
+      setToken(_token)
+
+    spotify.setAccessToken(_token)
+    spotify.getMe().then(user => {
+      console.log(user)
+    })
   }, [])
 
   return (
     <div className="app">
       {/* Spotify Logo */}
       {/* Login with spotify button */}
-      <Login />
+      {
+        token ? (
+
+          <Player />
+        ) :
+          (
+            <Login />
+          )
+      }
     </div>
   );
 }
