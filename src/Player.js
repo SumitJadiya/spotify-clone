43ff227faea2ed6d380e2859d038bdfd8e653099
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-08-19 23:51:58 +0530

    create player

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./Player.css"
+import Sidebar from './Sidebar'
+import Body from './Body'
+import Footer from './Footer'
 
 function Player({ spotify }) {
     return (
-        <div>
-            <h1>Welcome to spotify</h1>
+        <div className="player">
+            <div class="player__body">
+                {/* Sidebar */}
+                <Sidebar />
+                {/* Body */}
+                <Body />
+            </div>
+            <Footer />
+            {/* Footer */}
         </div>
     )
 }
